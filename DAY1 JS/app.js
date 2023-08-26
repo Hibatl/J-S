621ceef8df76fe97bf08c3ba5952a5531f154df6
Hibatl <125480150+Hibatl@users.noreply.github.com>
2023-08-26 19:59:09 +0100

    Add files via upload

---
+++ DAY1 JS/app.js
@@ -0,0 +1,60 @@
+ex1:
+let myName = "Hiba" ;
+let age = 27 ;
+let isStudent = true ;
+let favoriteColors = [ blue, grey, green ] ;
+ex2:
+let myVar = "yes Madam" 
+let myVar = 'yes Madam'
+console.log (myVar);
+ex3:
+let myName = "Hiba" ;
+let age = 27 ;
+let isStudent = true ;
+let occupation = isStudent ? "a student" : "not a student";
+console.log = (`${myName} is and im ${age} years old , actually my accupation ${occupation}.`) ;
+ex5:
+const favoriteAnimal = prompt ("whats yr favorite Animal")
+const favoriteColor = prompt ("whats yr favorite color")
+const myAnimal = "dog"
+const myColor = "blue"
+if ( favoriteAnimal === "dog" && favoriteColor === "blue");
+console.log ("yes! its matchy")
+ex:6
+let number = prompt("enter a number:");
+// Convert the user input to a number using the unary plus operator (+)
+number = +number;
+
+if (number > 0) {
+    console.log("number is positive");
+
+} else if (number === 0) {
+    console.log ("number is equal to zero");
+
+} else if (number < 0) {
+    console.log ("number is negative");
+
+} else {
+    console.log("invalid");
+} 
+// didnt work!!
+ex7:
+
+const skyColor = prompt("whats the color of the Sky?")
+
+if (skyColor === "blue"){
+    console.log("true");
+}else{
+    console.log("false")
+}
+
+
+
+
+
+
+
+
+
+
+

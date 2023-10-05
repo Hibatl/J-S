b4fce47803e77e9ab2d26d618d4e509864a6b0b7
Hibatl <125480150+Hibatl@users.noreply.github.com>
2023-10-05 10:24:26 +0100

    Add files via upload

---
+++ day21.js
@@ -0,0 +1,23 @@
+function addNumbers(a, b) {
+    console.log(a + b);
+  }
+  
+ 
+  function subtractNumbers(a, b) {
+    console.log(a - b);
+  }
+  
+  
+  function multiplyNumbers(a, b) {
+    console.log(a * b);
+  }
+  
+  
+  function divideNumbers(a, b) {
+    if (b === 0) {
+    console.log("Cant be divided by 0");
+    }
+    console.log(a / b);
+  }
+  
+
